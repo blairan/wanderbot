e1dc40280fc4dc507cc6a62d7af3bd06638e73cc
blairan <blairan121122885@gmail.com>
2024-06-16 10:24:20 +0800

    add integrate camera

--- src/wanderbot_integrate/wanderbot_integrate/launch/wanderbot_integration.launch.py
+++ src/wanderbot_integrate/wanderbot_integrate/launch/wanderbot_integration.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 import os
 
 def generate_launch_description():
@@ -21,8 +22,16 @@ def generate_launch_description():
         ros2_stm32_bridge_file
     ]))
 
+    #相機啟動
+    camera_action = Node(
+        package= "usb_cam",
+        executable= "usb_cam_node_exe",
+        arguments= ["--ros-args", "--params-file", "src/wanderbot_integrate/camera_config/usb_cam.yaml"]
+    )
+
 
     return LaunchDescription([
         wanderbot_action,
-        ros2_stm32_bridge
+        ros2_stm32_bridge,
+        camera_action
     ])
